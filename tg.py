0913053003a7232a7c63dc72241bea505080921c
Aayushx4513 <thakor97233@gmail.com>
2026-05-29 14:46:37 +0530

    your message here

--- tg.py
+++ tg.py
@@ -22,7 +22,7 @@ def home():
     return "Bot is running!"
 
 def run_flask():
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     flask_app.run(host="0.0.0.0", port=port)
 
 def get_db():
